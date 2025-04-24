4637780c3902148a2476bf8b548c8724050dccb1
ogulcan@permentostudio.nl <ogulcan@permentostudio.nl>
2025-04-24 13:54:29 +0000

    Created Form1.ItemTemplate2; Edited Form1, Form1.ItemTemplate1, Form1.RowTemplate1, Form1.RowTemplate2 and 1 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -27,19 +27,40 @@ class Form1(Form1Template):
         start_date = date(new_year, new_month, 1)
         self.start_date.date = start_date
         self.num_startdate = self.strdate_to_numdate(self.start_date.date)
+        self.data_grid_4.rows_per_page = 20
       
         self.sort_state_1 = {}
         self.link_map = {
             'column_1': self.link_1,
             'column_2': self.link_2,
             'column_3': self.link_3,
-            'column_4': self.link_4
+            'column_4': self.link_4,
+            'column_5': self.link_5,
+            'column_6': self.link_6,
+            'column_7': self.link_7,
+            'column_8': self.link_8,
+            'column_9': self.link_9,
+            'column_10': self.link_10,
+            'column_11': self.link_11,
+            'column_12': self.link_12,
+            'column_13': self.link_13,
+            'column_14': self.link_14
         }
 
         self.link_1.icon = 'fa:caret-down'
         self.link_2.icon = 'fa:sort'
         self.link_3.icon = 'fa:sort'
         self.link_4.icon = 'fa:sort'
+        self.link_5.icon = 'fa:caret-down'
+        self.link_6.icon = 'fa:sort'
+        self.link_7.icon = 'fa:sort'
+        self.link_8.icon = 'fa:sort'
+        self.link_9.icon = 'fa:sort'
+        self.link_10.icon = 'fa:caret-down'
+        self.link_11.icon = 'fa:sort'
+        self.link_12.icon = 'fa:sort'
+        self.link_13.icon = 'fa:sort'
+        self.link_14.icon = 'fa:sort'
 
     def strdate_to_numdate(self, strdate):
         return datetime.combine(strdate, datetime.min.time())
@@ -50,8 +71,11 @@ class Form1(Form1Template):
     def form_refreshing_data_bindings(self, **event_args):
         pass
 
-    def search_box_pressed_enter(self, **event_args):
-        self.filter_handelingen()
+    def search_box_1_change(self, **event_args):
+        self.filter_email_stats()  # Filter voor repeating_panel_1
+
+    def search_box_4_change(self, **event_args):
+        self.filter_assessor_info()
 
     def search_box_change(self, **event_args):
         self.filter_handelingen()
@@ -62,7 +86,6 @@ class Form1(Form1Template):
         self.plot_2_show()
         self.plot_3_show()
         self.data_grid_1_show()
-        self.data_grid_2_show()
         self.data_grid_3_show()
         self.data_grid_4_show()
 
@@ -72,7 +95,6 @@ class Form1(Form1Template):
         self.plot_2_show()
         self.plot_3_show()
         self.data_grid_1_show()
-        self.data_grid_2_show()
         self.data_grid_3_show()
         self.data_grid_4_show()
 
@@ -157,10 +179,18 @@ class Form1(Form1Template):
         )
 
     def data_grid_1_show(self, **event_args):
-        self.repeating_panel_1.items = anvil.server.call('get_email_stats', self.num_startdate, self.num_enddate)
+        # Haal de data op van de server
+        data = anvil.server.call('get_email_stats', self.num_startdate, self.num_enddate)
 
-    def data_grid_2_show(self, **event_args):
-        self.repeating_panel_2.items = anvil.server.call('get_assessor_info', self.num_startdate, self.num_enddate)
+        # Controleer of de opgehaalde data een lijst is
+        if isinstance(data, list):
+            # Stel de items in als de data
+            self.repeating_panel_1.items = data
+            self.repeating_panel_1.tag = data  # Bewaar de data in de tag als een lijst
+        else:
+            # Als de data geen lijst is, stel dan een lege lijst in
+            self.repeating_panel_1.items = []
+            self.repeating_panel_1.tag = []
 
     def data_grid_3_show(self, **event_args):
         handeling_result = anvil.server.call('get_handeling_stats', self.num_startdate, self.num_enddate)
@@ -169,14 +199,14 @@ class Form1(Form1Template):
         self.repeating_panel_3.tag = sorted_data
 
     def data_grid_4_show(self, **event_args):
-      try:
-          data = anvil.server.call('get_assessor_info', self.num_startdate, self.num_enddate)
-          sorted_data = sorted(data, key=lambda x: str(x['achternaam']).lower())
-          self.repeating_panel_4.items = sorted_data
-          self.repeating_panel_4.tag = sorted_data
-      except Exception as e:
-          print(f"Fout bij het ophalen van assessor informatie: {e}")
-          alert(f"Fout bij het ophalen van assessor informatie: {e}")
+        try:
+            data = anvil.server.call('get_assessor_info', self.num_startdate, self.num_enddate)
+            sorted_data = sorted(data, key=lambda x: str(x['achternaam']).lower())
+            self.repeating_panel_4.items = sorted_data
+            self.repeating_panel_4.tag = sorted_data
+        except Exception as e:
+            print(f"Fout bij het ophalen van assessor informatie: {e}")
+            alert(f"Fout bij het ophalen van assessor informatie: {e}")
 
     def filter_handelingen(self, **event_args):
         zoekterm = self.search_box.text.lower()
@@ -196,7 +226,50 @@ class Form1(Form1Template):
 
         self.repeating_panel_3.items = gefilterde_resultaten
 
+    def filter_email_stats(self, **event_args):
+        zoekterm = self.search_box_1.text.lower()  # Aangepast zoekvak voor repeating_panel_1
+        originele_data_1 = self.repeating_panel_1.tag
+
+        # Controleer of de tag geen None is en of het een lijst is
+        if not isinstance(originele_data_1, list):
+            return  # Als de tag geen lijst is, stop dan
+
+        if originele_data_1 is None:
+            return
+
+        if not zoekterm or len(zoekterm) < 2:
+            self.repeating_panel_1.items = originele_data_1
+            return
+
+        # Filter de resultaten op basis van de zoekterm
+        gefilterde_resultaten_1 = []
+        for item in originele_data_1:
+            if item.get('achternaam', '').lower().startswith(zoekterm) or item.get('voornaam', '').lower().startswith(zoekterm):
+                gefilterde_resultaten_1.append(item)
+
+        # Stel de gefilterde items in
+        self.repeating_panel_1.items = gefilterde_resultaten_1
+
+    def filter_assessor_info(self, **event_args):
+        zoekterm = self.search_box_4.text.lower()  # Aangepast zoekvak voor repeating_panel_4
+        originele_data_4 = self.repeating_panel_4.tag
+
+        if originele_data_4 is None:
+            return
+
+        if not zoekterm or len(zoekterm) < 2:
+            self.repeating_panel_4.items = originele_data_4
+            return
+
+        gefilterde_resultaten_4 = []
+        for item in originele_data_4:
+            if item.get('achternaam', '').lower().startswith(zoekterm) or item.get('voornaam', '').lower().startswith(zoekterm):
+                gefilterde_resultaten_4.append(item)
+
+        self.repeating_panel_4.items = gefilterde_resultaten_4
+
     def link_click(self, **event_args):
+        # Toggle icon
         if event_args['sender'].icon == 'fa:caret-down':
             event_args['sender'].icon = 'fa:caret-up'
         elif event_args['sender'].icon == 'fa:caret-up':
@@ -210,25 +283,58 @@ class Form1(Form1Template):
                 clicked_column = column
                 break
 
+        # Reset icons of other links
         for link in self.link_map.values():
             if link != event_args['sender']:
                 link.icon = 'fa:sort'
 
-        current_data = self.repeating_panel_3.items
+        reverse = event_args['sender'].icon == 'fa:caret-up'
 
-        if clicked_column == 'column_1':
-            sorted_data = sorted(current_data, key=lambda k: str(k['handeling']).lower(), reverse=event_args['sender'].icon == 'fa:caret-up')
-        elif clicked_column == 'column_2':
-            sorted_data = sorted(current_data, key=lambda k: k['aantal_handeling_checks'], reverse=event_args['sender'].icon == 'fa:caret-up')
-        elif clicked_column == 'column_3':
-            sorted_data = sorted(current_data, key=lambda k: k['aantal_failed'], reverse=event_args['sender'].icon == 'fa:caret-up')
-        elif clicked_column == 'column_4':
-            sorted_data = sorted(current_data, key=lambda k: k['aantal_passed'], reverse=event_args['sender'].icon == 'fa:caret-up')
-        else:
-            sorted_data = current_data
+        # Sort repeating_panel_3 (Handelingen)
+        if clicked_column in ['column_1', 'column_2', 'column_3', 'column_4']:
+            current_data = self.repeating_panel_3.items
 
-        self.repeating_panel_3.items = sorted_data
+            if clicked_column == 'column_1':
+                sorted_data = sorted(current_data, key=lambda k: str(k['handeling']).lower(), reverse=reverse)
+            elif clicked_column == 'column_2':
+                sorted_data = sorted(current_data, key=lambda k: k['aantal_handeling_checks'], reverse=reverse)
+            elif clicked_column == 'column_3':
+                sorted_data = sorted(current_data, key=lambda k: k['aantal_failed'], reverse=reverse)
+            elif clicked_column == 'column_4':
+                sorted_data = sorted(current_data, key=lambda k: k['aantal_passed'], reverse=reverse)
 
-    
+            self.repeating_panel_3.items = sorted_data
 
+        # Sort repeating_panel_1 (Email stats)
+        elif clicked_column in ['column_5', 'column_6', 'column_7', 'column_8', 'column_9']:
+            current_data = self.repeating_panel_1.items
 
+            if clicked_column == 'column_5':
+                sorted_data = sorted(current_data, key=lambda k: str(k['achternaam']).lower(), reverse=reverse)
+            elif clicked_column == 'column_6':
+                sorted_data = sorted(current_data, key=lambda k: k['voornaam'], reverse=reverse)
+            elif clicked_column == 'column_7':
+                sorted_data = sorted(current_data, key=lambda k: k['email-adres'], reverse=reverse)
+            elif clicked_column == 'column_8':
+                sorted_data = sorted(current_data, key=lambda k: k['locatie'], reverse=reverse)
+            elif clicked_column == 'column_9':
+                sorted_data = sorted(current_data, key=lambda k: k['count'], reverse=reverse)
+
+            self.repeating_panel_1.items = sorted_data
+
+        # Sort repeating_panel_4 (Assessor info)
+        elif clicked_column in ['column_10', 'column_11', 'column_12', 'column_13', 'column_14']:
+            current_data = self.repeating_panel_4.items
+
+            if clicked_column == 'column_10':
+                sorted_data = sorted(current_data, key=lambda k: str(k['achternaam']).lower(), reverse=reverse)
+            elif clicked_column == 'column_11':
+                sorted_data = sorted(current_data, key=lambda k: k['voornaam'], reverse=reverse)
+            elif clicked_column == 'column_12':
+                sorted_data = sorted(current_data, key=lambda k: k['email-adres'], reverse=reverse)
+            elif clicked_column == 'column_13':
+                sorted_data = sorted(current_data, key=lambda k: k['locatie'], reverse=reverse)
+            elif clicked_column == 'column_14':
+                sorted_data = sorted(current_data, key=lambda k: k['count'], reverse=reverse)
+
+            self.repeating_panel_4.items = sorted_data
